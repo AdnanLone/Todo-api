9cbfa4bbb5813aa07d18d4ffa5d86a1b9ed1d507
Adnan Ali <adnanlone13@gmail.com>
2016-04-05 18:15:57 -0400

    adding hash and salt passwords

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post('/users', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function (user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(400).json(e);
 	});
@@ -125,7 +125,7 @@ app.put('/todos/:id', function(req, res) {
 	});
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: false}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on PORT ' + PORT + ' !');
 	});
