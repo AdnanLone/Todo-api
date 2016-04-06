50cf3a0ece2cb755eea2e02d1841bea12ce94c79
Adnan Ali <adnanlone13@gmail.com>
2016-04-06 00:27:06 -0400

    adding login route and authenticate class methods

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [];
-var todoNextId = 1;
+
+// var todos = [];
+// var todoNextId = 1;
 
 app.use(bodyParser.json());
 
@@ -67,16 +69,6 @@ app.post('/todos', function(req, res) {
 	});
 });
 
-app.post('/users', function (req, res) {
-	var body = _.pick(req.body, 'email', 'password');
-
-	db.user.create(body).then(function (user) {
-		res.json(user.toPublicJSON());
-	}, function (e) {
-		res.status(400).json(e);
-	});
-});
-
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id);
 
@@ -125,7 +117,33 @@ app.put('/todos/:id', function(req, res) {
 	});
 });
 
-db.sequelize.sync({force: false}).then(function() {
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function(user) {
+		res.json(user.toPublicJSON());
+	}, function(e) {
+		res.status(400).json(e);
+	});
+});
+
+//POST / users/login
+
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function (e) {
+		res.status(401).send();
+	});
+});
+
+
+
+db.sequelize.sync({
+	force: true
+}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on PORT ' + PORT + ' !');
 	});
